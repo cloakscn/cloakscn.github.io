81d1dc4347422a7447ae7eded12cbb3f725cf108
王艺 <wy0329@foxmail.com>
2022-03-13 20:31:19 +0800

    image

---
+++ docs/assets/Layout.da483470.js
@@ -0,0 +1,1 @@
+import{g as r,m,j as a,r as e,n,p as s}from"./app.c052f68f.js";import{S as p}from"./SkipLink.20f25e53.js";var i=r({name:"Layout",setup(){const o=s(),t=m();return()=>[a(p),a(e("CommonWrapper"),()=>t.value.home?a(e("HomePage")):a(n,()=>a(e("NormalPage"),{key:o.value.path})))]}});export{i as default};
