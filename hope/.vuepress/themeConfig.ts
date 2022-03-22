5d5ee21077a086ee0582298f88cf2f19fa8bdb06
王艺 <wy0329@foxmail.com>
2022-03-22 19:52:13 +0800

    1. 添加 doc search

--- hope/.vuepress/themeConfig.ts
+++ hope/.vuepress/themeConfig.ts
@@ -148,19 +148,68 @@ export default defineThemeConfig({
       autoExcerpt: true,
     },
     
-    search: {
+    // search: {
+    //   locales: {
+    //     '/en/': {
+    //       placeholder: 'Search',
+    //     },
+    //     '/': {
+    //       placeholder: '搜索',
+    //     },
+    //   },
+    //   hotKeys: ['s', '/'],
+    //   maxSuggestions: 5,
+    //   isSearchable: (page) => page.path !== '/',
+    //   getExtraFields: (page) => page.frontmatter.tags ?? [],
+    // },
+
+    docsearch: {
+      appId: "KSKSOSLFVW",
+      apiKey: "d6bdc520b47969a0d59af06c01d17632",
+      indexName: "cloaks",
       locales: {
-        '/en/': {
-          placeholder: 'Search',
-        },
-        '/': {
-          placeholder: '搜索',
+        "/": {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                openIssueText: "你认为该查询应该有结果？",
+                openIssueLinkText: "点击反馈",
+              },
+            },
+          },
         },
       },
-      hotKeys: ['s', '/'],
-      maxSuggestions: 5,
-      isSearchable: (page) => page.path !== '/',
-      getExtraFields: (page) => page.frontmatter.tags ?? [],
     },
 
     mdEnhance: {
