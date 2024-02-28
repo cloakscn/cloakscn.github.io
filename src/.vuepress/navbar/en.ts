94ff2b806c19038efdea543c0ac03d51034a2cec
王艺 <wy03290019@gmail.com>
2024-02-28 15:54:29 +0800

    update en navbar && sidebar

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -2,6 +2,11 @@ import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
   "/",
+  {
+    text: "Articles",
+    icon: "pen-to-square",
+    link: "posts/"
+  },
   // "/demo/",
   // {
   //   text: "Posts",
--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -9,11 +9,5 @@ export const enSidebar = sidebar({
       prefix: "posts/",
       children: "structure",
     },
-    // "intro",
-    // {
-    //   text: "Slides",
-    //   icon: "person-chalkboard",
-    //   link: "https://plugin-md-enhance.vuejs.press/guide/content/revealjs/demo.html",
-    // },
   ],
 });
