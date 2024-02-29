75d12bbf623341bf950d99ccf263b3d28a9452d7
王艺 <wy03290019@gmail.com>
2024-02-29 10:33:01 +0800

    update sidebar && navbar

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -4,16 +4,23 @@ export const zhSidebar = sidebar({
   "/zh/": [
     "",
     {
+      text: "文章",
+      icon: "book",
+      prefix: "posts/",
+      children: "structure",
+    },
+  ],
+  "/zh/design-patterns/": [
+    {
       text: "设计模式",
       icon: "book",
-      collapsible: true,
-      prefix: "design-patterns/",
-      link: "design-patterns/",
+      link: "/zh/design-patterns/",
+      collapsible: false,
       children: [
         {
           text: "行为模式",
           icon: "book",
-          collapsible: true,
+          collapsible: false,
           prefix: "behavioral-patterns/",
           link: "behavioral-patterns/",
           children: "structure",
@@ -21,7 +28,7 @@ export const zhSidebar = sidebar({
         {
           text: "创建型模式",
           icon: "book",
-          collapsible: true,
+          collapsible: false,
           prefix: "creational-patterns/",
           link: "creational-patterns/",
           children: "structure",
@@ -29,57 +36,47 @@ export const zhSidebar = sidebar({
         {
           text: "建造者模式",
           icon: "book",
-          collapsible: true,
+          collapsible: false,
           prefix: "structural-patterns/",
           link: "structural-patterns/",
           children: "structure",
         },
       ],
-    },
-    // {
-    //   text: "重构",
-    //   icon: "book",
-    //   collapsible: true,
-    //   prefix: "refactoring/",
-    //   link: "refactoring/",
-    //   children: [
-    //     {
-    //       text: "什么是重构？",
-    //       icon: "book",
-    //       collapsible: true,
-    //       prefix: "what-is-refactoring/",
-    //       link: "what-is-refactoring/",
-    //       children: "structure",
-    //     },
-    //     {
-    //       text: "代码特征",
-    //       icon: "book",
-    //       collapsible: true,
-    //       prefix: "code-smells/",
-    //       link: "code-smells/",
-    //       children: "structure",
-    //     },
-    //     {
-    //       text: "解决方案",
-    //       icon: "book",
-    //       collapsible: true,
-    //       prefix: "techniques/",
-    //       link: "techniques/",
-    //       children: "structure",
-    //     },
-    //   ]
-    // },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    // "intro",
-    // {
-    //   text: "幻灯片",
-    //   icon: "person-chalkboard",
-    //   link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    // },
+    }
   ],
-});
+  "/zh/refactoring/": [
+    {
+      text: "重构",
+      icon: "book",
+      link: "/zh/refactoring/",
+      collapsible: false,
+      children: [
+        {
+          text: "什么是重构",
+          icon: "book",
+          collapsible: false,
+          prefix: "what-is-refactoring/",
+          link: "what-is-refactoring/",
+          children: "structure",
+        },
+        {
+          text: "代码特征",
+          icon: "book",
+          collapsible: false,
+          prefix: "code-smells/",
+          link: "code-smells/",
+          children: "structure",
+        },
+        {
+          text: "解决方案",
+          icon: "book",
+          collapsible: false,
+          prefix: "techniques/",
+          link: "techniques/",
+          children: "structure",
+        },
+      ]
+    }
+  ],
+},
+);
